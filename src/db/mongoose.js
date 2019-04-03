25202383b0fe7c8b268b8ee74212ae532e03f86d
H1 <hayouniabdessattar@gmail.com>
2019-04-03 16:23:50 -0600

    'dbtest'

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/angela', {
+const dbURL=process.env.dbURL||'mongodb://127.0.0.1:27017/angela'
+mongoose.connect(dbURL, {
     useNewUrlParser: true,
     useCreateIndex: true
 })
